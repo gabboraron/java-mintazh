2965a2bc29af64d0e56ba9ac37f36c823cd25086
gabboraron <gabboraron@gmail.com>
2017-12-29 19:01:14 +0100

    Add files via upload

---
+++ market/Fruit.java
@@ -0,0 +1,109 @@
+import java.util.*;
+import java.io.*;
+
+public class Fruit{
+	private String name;
+	private int price;
+	
+	//KONSTRUKTOR
+	public Fruit (String name, int price){
+		this.name = name;
+		this.price = price;
+		
+		//System.out.println("NAME/PRICE SET");
+		//System.out.println("getCheapestFruit(): " + getCheapestFruit());
+		//System.out.println("getCheapestFruit(): " + getCheapestFruit().price);
+		//System.out.println("getCheapestFruit(): " + getCheapestFruit().name);
+		//System.out.println("getCheapestFruit().show() " + getCheapestFruit().show());
+		
+		/*if (getCheapestFruit() == null){
+			setCheapestFruit(name, price);
+			System.out.println("CHEAPEST NAME/PRICE INITIALIZED");
+		} else {
+			if(getCheapestFruit().price> price){
+				setCheapestFruit(name, price);
+				System.out.println("CHEAPEST NAME/PRICE SET");
+			}			
+		}*/
+	}
+	
+	public int getPrice(){
+		return this.price;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public boolean cheaperThan(int currentFruitPrice){
+		return this.price > currentFruitPrice;
+	}
+	
+	public static Fruit make(String name, int price){
+		if((name.length() > 1) && (name.matches("[a-zA-Z]+"))){
+			System.out.println("NAME OK");
+			if((price<5001) && (price>0) && (((price % 2) == 0) || ((price % 2) == 5))){
+				System.out.println("FRUIT CREATED");
+				setCheapestFruit(name, price);
+				return new Fruit(name, price);
+			}
+			System.out.println("PRICE ERROR");
+		}
+		System.out.println("NAME ERROR");
+		return null;
+	}
+	
+	public String show(){
+		String tmpPrice = String.valueOf(this.price);
+		if(name.length()>3){
+			tmpPrice = "";
+			String tmpChar = "";
+			int counter = 0;
+			for(int i = String.valueOf(this.price).length()-1; i>-1; --i){
+				if(counter == 3 ){
+					tmpPrice = " " + tmpPrice;
+					counter = 0;
+				}
+				tmpPrice = String.valueOf(this.price).charAt(i) + tmpPrice;
+				++counter;
+			}
+		}
+		return this.name + " (" + tmpPrice + " Ft)";
+	} 
+	
+	public static Fruit cheapestFruit = null;
+	public static Fruit getCheapestFruit(){
+		return cheapestFruit;
+	}
+	public static void setCheapestFruit(String name, int price){
+		if (getCheapestFruit() == null){
+			cheapestFruit = new Fruit (name, price);
+			//System.out.println("CHEAPEST NAME/PRICE INITIALIZED");
+		} else {
+			if(getCheapestFruit().price> price){
+				cheapestFruit = new Fruit (name, price);
+				//setCheapestFruit(name, price);
+				//System.out.println("CHEAPEST NAME/PRICE SET");
+			}			
+		}
+		
+		/*this.cheapestFruit.price = price;
+		//this.cheapestFruit = new Fruit(name,price);
+		System.out.println("***");
+		//this.cheapestFruit.name = name;*/
+	}
+	
+	public static void main(String[] args){
+		//Fruit myFruit = new Fruit(args[0],Integer.parseInt(args[1]));
+		Fruit myFruit = make(args[0],Integer.parseInt(args[1]));
+		System.out.println(myFruit.show());
+		//System.out.print("name:" + myFruit.getName() + " ");
+		//System.out.println("price:" + myFruit.getPrice());
+		Fruit mySndFruit = make("banan",300);
+		//System.out.print("name:" + mySndFruit.getName() + " ");
+		//System.out.println("price:" + mySndFruit.getPrice());
+		System.out.println(mySndFruit.show());
+		System.out.println("CHEAPEST: " + mySndFruit.getCheapestFruit().show());
+	}
+	
+}
